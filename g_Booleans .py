e05412b6d071eebf6de323660d5b7c6b40fcd27e
Shoptorshi Kormoker Chumky <shopto.sk@gmail.com>
2025-01-07 23:53:43 +0600

    day-1:booleans

---
+++ g_Booleans .py
@@ -0,0 +1,14 @@
+#In fact, there are not many values that evaluate to False, except empty values, such as (), [], {}, "", the number 0, and the value None. And of course the value False evaluates to False#
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
+print(bool(False))
+print(bool(None))
+print(bool(0))
+print(bool(""))
+print(bool(()))
+print(bool([]))
+print(bool({}))
+
