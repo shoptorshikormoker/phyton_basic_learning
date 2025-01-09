8af6d84f97e6e4025de3795f2944ec38bbd0b564
Shoptorshi Kormoker Chumky <shopto.sk@gmail.com>
2025-01-09 17:13:38 +0600

    day_3string modify

--- f_string.py
+++ f_string.py
@@ -9,14 +9,49 @@ a multi-line string'''
 print(string1[0])  # output: 'H' (first character)
 print(string2[-1]) # output: 'd' (last character)
 
-# Slicing
-print(string1[1:4])  # 'ell'
+# Slicing : Get the characters from position 2 to position 5 (not included):
 
-# Concatenation
+b = "Hello, World!"
+print(b[2:5])
+"""
+negativeindexing
+
+Get the characters from the start to position 5 (not included):
+
+b = "Hello, World!"
+print(b[:5])
+
+Negative Indexing
+Use negative indexes to start the slice from the end of the string:
+
+Get the characters:
+
+From: "o" in "World!" (position -5)
+
+To, but not included: "d" in "World!" (position -2):
+"""
+b = "Hello, World!"
+print(b[-5:-2])
+
+
+# Concatenation :To concatenate, or combine, two strings you can use the + operator.
+
 print(string1 + ' ' + string2)  # 'Hello World'
 
 # Repetition
 print(string1 * 3)  # 'HelloHelloHello'
 
+
 # Length
 print(len(string1))  # 5
+
+Placeholders and Modifiers: Add a placeholder for the price variable:
+
+price = 59
+txt = f"The price is {price} dollars"
+print(txt)
+
+Escape characters : To fix this problem, use the escape character \" The escape character allows you to use double quotes when you normally would not be allowed:
+
+txt = "We are the so-called \"Vikings\" from the north."
+print(txt) 
